6e625041d01121ac60bd3b05739e45c3e057719f
Jan Kirchner <kirchner.jan@icloud.com>
2018-02-23 12:17:16 +0100

    Scripted auto-commit on change (2018-02-23 12:17:15) by gitwatch.sh

--- importCSV.m
+++ importCSV.m
@@ -95,7 +95,6 @@ function [readIn ,skip] = importCSV(csvPath , params , flags)
             F0means = means(:,mask); readIn.F0quant = mean(F0means(:));
             means(:,mask) = []; readIn.pos(mask,:) = [];
         end
-        scatter(readIn.pos(:,2),-readIn.pos(:,1),'filled')
         idMat = pdist2(readIn.pos,readIn.pos)+eye(size(readIn.pos,1))*100 < params.idLim;
         idMat = idMat - triu(idMat);
         [row , col] = find(idMat);
