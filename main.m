f2b8b193219e4630dadcd25c142a65e8e4f6781b
Jan Kirchner <kirchner.jan@icloud.com>
2018-02-23 14:02:18 +0100

    Scripted auto-commit on change (2018-02-23 14:02:18) by gitwatch.sh

--- main.m
+++ main.m
@@ -463,7 +463,7 @@ function [] = barParRate(tab,params)
 
     end
     
-    bar(ampBar);errorbar()
+    bar(ampBar);errorbar(ampBar,ampStd);
     xticklabels({'0-20%','20-40%','40-60%','60-80%','80-100%'});
     xlabel('Participation rate')
     ylabel('Amplitude (F/F0)')
