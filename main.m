02c574b2586f4e01a29e93efd5d79bd623994a6b
Jan Kirchner <kirchner.jan@icloud.com>
2018-02-23 13:30:39 +0100

    Scripted auto-commit on change (2018-02-23 13:30:39) by gitwatch.sh

--- main.m
+++ main.m
@@ -868,7 +868,7 @@ end
 function [] = scatterAmpJitter(tab,params,flags , data)
     figure(); 
     subplot(1,2,1);
-    colormap(jet);scatter(tab.rates,tab.amps,params.ScatterPointSize,tab.durations)%,'MarkerFaceColor',rgb('gray'),'MarkerEdgeColor',rgb('black')); 
+    colormap(jet);scatter(tab.rates,tab.amps,params.ScatterPointSize,tab.durationsFWHM)%,'MarkerFaceColor',rgb('gray'),'MarkerEdgeColor',rgb('black')); 
     colorbar
     ylabel('Amplitude ^{F}/_{F0}'); xlim([0,1]); %ylim([0.8,1.6])
     xlabel('Participation rate (%)');% colorbar
