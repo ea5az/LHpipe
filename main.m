1d9cb03933fc4b5d9ab6e2e8391e89b292edb235
Jan Kirchner <kirchner.jan@icloud.com>
2018-02-24 14:17:18 +0100

    Scripted auto-commit on change (2018-02-24 14:17:18) by gitwatch.sh

--- main.m
+++ main.m
@@ -679,9 +679,11 @@ function [] =  IEIs(tab , params)
     Hbins(2:2:end) = NHIEI;
     dt = (dx(2)-dx(1))/2;
     h1 = histogram('BinEdges',dx,'BinCounts',Lbins(1:end-1),'Normalization','Probability','FaceColor',rgb('darkgray'))
-    errorbar(h1.BinEdges(1:2:end-1)+dt, h1.Values(1:2:end), MultinomialConfidenceIntervals(h1.BinCounts(1:2:end),0.05),'x')
+    [~,b1] = MultinomialConfidenceIntervals(h1.BinCounts(1:2:end),0.05);
+    errorbar(h1.BinEdges(1:2:end-1)+dt, h1.Values(1:2:end),b1(1,:),b1(2,:) ,'x')
     h2 = histogram('BinEdges',dx,'BinCounts',Hbins(1:end-1),'Normalization','Probability','FaceColor',rgb('red'))
-    errorbar(h2.BinEdges(2:2:end-1)+dt, h2.Values(2:2:end), MultinomialConfidenceIntervals(h2.BinCounts(2:2:end),0.05),'x')
+    [~,b2] = MultinomialConfidenceIntervals(h2.BinCounts(1:2:end),0.05);
+    errorbar(h2.BinEdges(2:2:end-1)+dt, h2.Values(2:2:end),b1(1,:),b1(2,:),'x')
 
     xlim([0,300])
     legend({'Participation 20-80%','Participation 80-100%'})
