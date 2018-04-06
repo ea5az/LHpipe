bae1201dd5a7271aa51f864336757ff073217553
Jan Kirchner <kirchner.jan@icloud.com>
2018-04-06 15:26:42 +0200

    Scripted auto-commit on change (2018-04-06 15:26:42) by gitwatch.sh

--- combineFolder.m
+++ combineFolder.m
@@ -28,7 +28,7 @@ function [tab,pcaTab,corTab,cpdTab] = combineFolder(pathTo, params , flags)
     cID = NaN; cCond = NaN;
     
     LHrat = [];
-    LHratCell = cell(1,1);
+    LHratCell = {};
     for ii = 1:length(fileList)
         fileStr = fileList{ii}
         % extract expriment identifiers
