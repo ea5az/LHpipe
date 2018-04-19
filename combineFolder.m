a9d3166c4d2eed37af3a3a4b65fbfa9439406a48
Jan Kirchner <kirchner.jan@icloud.com>
2018-04-19 14:54:33 +0200

    Scripted auto-commit on change (2018-04-19 14:54:33) by gitwatch.sh

--- combineFolder.m
+++ combineFolder.m
@@ -37,14 +37,17 @@ function [tab,pcaTab,corTab,cpdTab] = combineFolder(pathTo, params , flags)
         % get the average activity from the ROI's.
         [readIn ,skip] = importCSV([pathTo fileStr] , params , flags);
         
-        save(fullfile('mat/PALOTS/',[fileStr(1:end-4) , '_READIN.mat']) , 'readIn')
-        ms = readIn.means;
-        save(fullfile('mat/PALOTS/',[fileStr(1:end-4) , '_MEANS.mat']) , 'ms')
 
         % If bad animal or too many ROIs
         if skip || size(readIn.means,2) > params.upperN
             continue 
         end
+        
+        save(fullfile('mat/PALOTS/',[fileStr(1:end-4) , '_READIN.mat']) , 'readIn')
+        ms = readIn.means;
+        save(fullfile('mat/PALOTS/',[fileStr(1:end-4) , '_MEANS.mat']) , 'ms')
+
+        
         % extract thresholds
         if flags.fixThreshPerExp
             if cID ~= expID || logical((cCond ~= readIn.cond)*flags.changeAtCondChange)
